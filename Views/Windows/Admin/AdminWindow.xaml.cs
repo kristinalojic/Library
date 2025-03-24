2a3341ee98173c29b77fe0b5f1d0d120111c42b7
Kristina <kristinalojic78@gmail.com>
2025-03-24 23:05:55 +0100

    Implementiran osnovni GUI interfejs

---
+++ Views/Windows/Admin/AdminWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Library.Views.Windows.Admin
+{
+    /// <summary>
+    /// Interaction logic for AdminWindow.xaml
+    /// </summary>
+    public partial class AdminWindow : Window
+    {
+        public AdminWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (sender is ListBox selectedListBox)
+            {
+                if (selectedListBox.SelectedItem == null)
+                    return;
+
+                ListBox1.SelectionChanged -= ListBox_SelectionChanged;
+                ListBox2.SelectionChanged -= ListBox_SelectionChanged;
+
+                if (selectedListBox == ListBox1)
+                {
+                    ListBox2.SelectedItem = null;
+                }
+                else if (selectedListBox == ListBox2)
+                {
+                    ListBox1.SelectedItem = null;
+                }
+
+                ListBox1.SelectionChanged += ListBox_SelectionChanged;
+                ListBox2.SelectionChanged += ListBox_SelectionChanged;
+            }
+        }
+    }
+}
---
+++ Views/Windows/Employee/MemberDetailsWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Library.Views.Windows.Employee
+{
+    /// <summary>
+    /// Interaction logic for MemberDetailsWindow.xaml
+    /// </summary>
+    public partial class MemberDetailsWindow : Window
+    {
+        public MemberDetailsWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
